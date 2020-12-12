30dd2f32b9b196b08be62be57f3cdf87888baf6a
DESKTOP-HMOQH1Q\Doni <ilikemylife16@gmail.com>
2020-12-12 12:31:27 +0700

    no message

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    Rigidbody2D Rb;
+    public float ms;
+    public float jf;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz = Input.GetAxisRaw("Horizontal");
+        Rb.velocity = new Vector2(ms * horiz, Rb.velocity.y);
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Rb.velocity = Vector2.up * jf;
+        }
+    }
+}
