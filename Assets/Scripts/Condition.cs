d78affd9f3764a358045381acf9a76fc5c684c66
LAPTOP-OO7BFUUT\Dimas <dimasrizkyaf@gmail.com>
2020-12-22 14:03:58 +0700

    no message

--- Assets/Scripts/Condition.cs
+++ Assets/Scripts/Condition.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Condition : MonoBehaviour
 {
+    public GameObject pausePanel;
+    public GameObject conditionPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pausePanel.SetActive(false);
+        conditionPanel.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    public void PauseButtonClicked()
     {
-        
+        pausePanel.SetActive(true);
+        conditionPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void ResumeButtonClicked()
+    {
+        pausePanel.SetActive(false);
+        conditionPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void RestartButtonClicked()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void MenuButtonClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
